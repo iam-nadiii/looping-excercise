c5a616bf0a806182278879cd4dbae03e6a18b87e
iam-nadiii <naodd43@gmail.com>
2026-04-15 11:08:40 -0500

    finished the WhileLoop exercise

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        while(counter < 5){
+            System.out.println("I love Java!");
+            counter++;
+
+            try {
+                Thread.sleep(1000); // Pause for 1 second
+            } catch (InterruptedException e) {
+                // Standard practice is to restore the interrupted status
+                Thread.currentThread().interrupt();
+            }
+
+
+        }
+    }
+}
+
+
+
