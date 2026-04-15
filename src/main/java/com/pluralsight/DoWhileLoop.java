c0ac7164d146109059ffc5a0c36de117d01f551f
iam-nadiii <naodd43@gmail.com>
2026-04-15 11:11:54 -0500

    finished the DoWhileLoop exercise

--- src/main/java/com/pluralsight/DoWhileLoop.java
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
 public class DoWhileLoop {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        do{
+            System.out.println("I love Java");
+            counter++;
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+        } while(counter < 5);
+    }
 }
+
+
+
